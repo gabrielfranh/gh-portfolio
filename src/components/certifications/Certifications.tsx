049a92868483df1a83c26f200d6e6c00f823a96a
Gabriel Habermann <gabrielfranh@gmail.com>
2025-03-30 17:59:10 -0300

    Adicionando nova certificacao

--- src/components/certifications/Certifications.tsx
+++ src/components/certifications/Certifications.tsx
@@ -5,18 +5,27 @@ const Certifications = () => {
         <>
             <section id='certifications' className="certifications-container certifications-section">
                 <h1 className="certifications-title">Certifications</h1>
-                <a href='https://www.credly.com/badges/def112d5-c48f-495f-8c77-c1408c52d675/linked_in_profile' target='_blank'>
-                    <div className="certifications-grid">
-                        <div className="certification-card">
-                            <img 
-                                src="/src/assets/images/practitioner_badge.png" 
-                                alt="AWS Certification Badge" 
-                                className="certification-badge" 
-                            />
-                            <p className="certification-name">AWS Cloud Practitioner</p>
-                        </div>
+                <div className="certifications-grid">
+                    <a href='https://www.credly.com/badges/def112d5-c48f-495f-8c77-c1408c52d675/linked_in_profile' target='_blank'>
+                        
+                            <div className="certification-card">
+                                <img 
+                                    src="/src/assets/images/practitioner_badge.png" 
+                                    alt="AWS Certification Badge" 
+                                    className="certification-badge" 
+                                />
+                                <p className="certification-name">AWS Cloud Practitioner</p>
+                            </div>
+                    </a>
+                    <div className="certification-card">
+                        <img 
+                            src="/src/assets/images/AWS_Technicals_Essentials.png" 
+                            alt="AWS Technical Essentials" 
+                            className="certification-badge" 
+                        />
+                        <p className="certification-name">AWS Technicals Essentials</p>
                     </div>
-                </a>
+                </div>
             </section>
         </>
     )
