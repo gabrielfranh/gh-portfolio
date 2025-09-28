9f74d227b2f761af773f1e55011b364e9a54e890
Gabriel Habermann <gabrielfranh@gmail.com>
2025-09-28 18:30:47 -0300

    Refactor: New minimal and comfortable design of navbar, home section and background

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -10,17 +10,6 @@ const About = () => {
       <div className="about-content">
         <div className="about-left">
           <img src={me} alt="Gabriel Habermann" className="about-photo" loading="lazy" />
-          <div className="about-icons">
-            <a
-              href="https://www.linkedin.com/in/gabrielfranh/?locale=en_US"
-              target="_blank"
-            >
-              <i className="fa-brands fa-linkedin fa-2xl homeIcons"></i>
-            </a>
-            <a href="https://github.com/gabrielfranh" target="_blank">
-              <i className="fa-brands fa-github fa-2xl homeIcons"></i>
-            </a>
-          </div>
         </div>
         <div className="about-right">
           <h2 className="about-title">{t("navbar.about")}</h2>
