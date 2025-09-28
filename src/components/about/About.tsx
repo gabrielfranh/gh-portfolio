deb9127905834397611ec455945407f41052c7d8
Gabriel Habermann <gabrielfranh@gmail.com>
2025-09-28 18:47:37 -0300

    Refactor: Add modern and minimal design into about section

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,6 +1,5 @@
 import { useTranslation } from "react-i18next";
 import "../../styles/components/about/About.css";
-import me from "../../assets/images/me.jpg";
 
 const About = () => {
   const { t } = useTranslation();
@@ -8,10 +7,7 @@ const About = () => {
   return (
     <div id="about" className="about-section">
       <div className="about-content">
-        <div className="about-left">
-          <img src={me} alt="Gabriel Habermann" className="about-photo" loading="lazy" />
-        </div>
-        <div className="about-right">
+        <div className="about-center">
           <h2 className="about-title">{t("navbar.about")}</h2>
           <p>{t("about.description")}</p>
         </div>
